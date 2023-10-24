f65dd404934de55fc464da362e2a56891d741e6d
Amanda DuKawa <110678035+madukisp@users.noreply.github.com>
2023-10-24 02:01:30 -0300

    Update PessoaFisicaRepo.java

--- src/model/PessoaFisicaRepo.java
+++ src/model/PessoaFisicaRepo.java
@@ -9,14 +9,16 @@ public class PessoaFisicaRepo {
     public void inserir(PessoaFisica pessoaFisica) {
         pessoasFisicas.add(pessoaFisica);
     }
+
     public void alterar(PessoaFisica pessoaFisica) {
-        for (int i =0; i < pessoasFisicas.size(); i++) {
+        for (int i = 0; i < pessoasFisicas.size(); i++) {
             if (pessoasFisicas.get(i).getId() == pessoaFisica.getId()) {
                 pessoasFisicas.set(i, pessoaFisica);
                 break;
             }
         }
     }
+
     public void excluir(int id) {
         pessoasFisicas.removeIf(p -> p.getId() == id);
     }
@@ -30,33 +32,23 @@ public class PessoaFisicaRepo {
         return null;
     }
 
-    public ArrayList<PessoaFisica> obeterTodos() {
+    public ArrayList<PessoaFisica> obterTodos() {
         return pessoasFisicas;
     }
 
     public void persistir(String nomeArquivo) throws IOException {
-        try {
-            FileOutputStream fileOut = new FileOutputStream(nomeArquivo);
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(pessoasFisicas);
-            out.close();
-            fileOut.close();
-            System.out.println("Dados de Pessoa Física Armazenados");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        FileOutputStream fileOut = new FileOutputStream(nomeArquivo);
+        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+        out.writeObject(pessoasFisicas);
+        out.close();
+        fileOut.close();
     }
 
     public void recuperar(String nomeArquivo) throws IOException, ClassNotFoundException {
-        try {
-            FileInputStream fileIn = new FileInputStream(nomeArquivo);
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            pessoasFisicas = (ArrayList<PessoaFisica>) in.readObject();
-            in.close();
-            fileIn.close();
-            System.out.println("Dados de Pessoa Física Recuperados");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        FileInputStream fileIn = new FileInputStream(nomeArquivo);
+        ObjectInputStream in = new ObjectInputStream(fileIn);
+        pessoasFisicas = (ArrayList<PessoaFisica>) in.readObject();
+        in.close();
+        fileIn.close();
     }
 }
